a6bef7bd0122f2ee17fd4277f13473b5731402ec
Dylan Frendo <dylanfrendo@gmail.com>
2024-12-10 23:48:18 +0900

    updated primitives

--- primitives/src/types.rs
+++ primitives/src/types.rs
@@ -3,20 +3,27 @@ use serde::{Deserialize, Serialize};
 use tendermint::block::Header;
 use tendermint_light_client_verifier::types::LightBlock;
 
-/// bytes32 trusted_header_hash;
-/// bytes32 target_header_hash;
-/// bytes32 data_commitment;
 /// uint64 trusted_block;
+/// bytes32 trusted_header_hash;
 /// uint64 target_block;
-/// uint256 validator_bitmap;
+/// bytes32 target_header_hash;
+/// bytes32 bridge_commitment;
 pub type ProofOutputs = sol! {
-    tuple(bytes32, bytes32, bytes32, uint64, uint64, uint256)
+    tuple(uint64, bytes32, uint64, bytes32, bytes32)
 };
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ProofInputs {
+    /// The trusted light block containing validator sets required for light client verification.
+    /// This block serves as the starting point for the light client's verification process.
     pub trusted_light_block: LightBlock,
+
+    /// The target light block to be verified against the trusted light block.
+    /// This block represents the endpoint of the verification process.
     pub target_light_block: LightBlock,
-    /// Exclusive of trusted_light_block and target_light_block's headers
+
+    /// Intermediate headers required to reconstruct the bridge commitment.
+    /// This vector contains all headers between (but not including) the trusted
+    /// and target light blocks' headers.
     pub headers: Vec<Header>,
 }
