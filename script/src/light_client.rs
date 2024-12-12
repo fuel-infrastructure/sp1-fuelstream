c2dc416dc0bcd05b0c379084885080e1e07a55b5
Dylan Frendo <dylanfrendo@gmail.com>
2024-12-12 19:16:31 +0900

    added binary search

--- script/src/light_client.rs
+++ script/src/light_client.rs
@@ -10,35 +10,29 @@ use tendermint_light_client::{
     },
 };
 
+use primitives::get_header_update_verdict;
+
 pub struct FuelStreamXLightClient {
     /// The default tendermint light client storage
     pub state: State,
     /// Handles the connection with a tendermint chain
     io: Box<dyn Io>,
-    /// The default tendermint light client verifier
-    verifier: Box<dyn Verifier>,
     /// Contains the tendermint chain options, including the trusting period and the trust threshold
     options: Options,
 }
 
 impl FuelStreamXLightClient {
     /// Constructs a new FuelStreamX light client
-    pub fn new(
-        io: impl Io + 'static,
-        verifier: impl Verifier + 'static,
-        state: State,
-        options: Options,
-    ) -> Self {
+    pub fn new(io: impl Io + 'static, state: State, options: Options) -> Self {
         Self {
             io: Box::new(io),
-            verifier: Box::new(verifier),
             state,
             options,
         }
     }
 
     /// Fetches a light block from a CometBFT node and stores it in the local state.
-    fn fetch_and_store_light_block(&mut self, block_height: u64, store: bool) -> LightBlock {
+    fn fetch_and_store_light_block(&mut self, block_height: u64, status: Status) -> LightBlock {
         debug!("fetching block {} from CometBFT", block_height);
 
         let block = self
@@ -46,53 +40,55 @@ impl FuelStreamXLightClient {
             .fetch_light_block(AtHeight::At(Height::try_from(block_height).unwrap()))
             .expect(&format!("could not request light block {}", block_height));
 
-        if store {
-            self.state
-                .light_store
-                .insert(block.clone(), Status::Verified);
-        }
+        self.state.light_store.insert(block.clone(), status);
 
         return block;
     }
 
     pub async fn get_next_block_sync(&mut self, start_block: u64, max_end_block: u64) -> u64 {
         debug!(
-            "finding next light client step between blocks {} and {}",
+            "finding the next light client header update between blocks {} and {}",
             start_block, max_end_block
         );
 
-        // Binary search to find the end block height
-        let trusted_block = self.fetch_and_store_light_block(start_block, true);
-        let mut curr_end_block = max_end_block;
-        loop {
-            // TODO: does this makes sense? it needs always + 1
-            if curr_end_block - start_block == 1 {
-                return curr_end_block;
+        // Store the trusted block for future uses
+        let trusted_block = self.fetch_and_store_light_block(start_block, Status::Verified);
+
+        // Finds the first untrusted block using binary search, if any
+        let mut left = start_block;
+        let mut right = max_end_block;
+        let mut last_trusted = left;
+
+        // Continue while there might be blocks between left and right
+        while left + 1 < right {
+            let mid = left + (right - left) / 2;
+
+            let untrusted_block = self.fetch_and_store_light_block(mid, Status::Unverified);
+
+            // Verification step
+            match get_header_update_verdict(&trusted_block, &untrusted_block) {
+                // If mid block is trusted, search in upper half
+                Verdict::Success => {
+                    last_trusted = mid;
+                    left = mid;
+
+                    // LightBlock is now trust-worthy
+                    self.state
+                        .light_store
+                        .insert(untrusted_block.clone(), Status::Verified);
+                }
+                // If mid block is not trusted, search in lower half
+                _ => {
+                    right = mid;
+
+                    self.state
+                        .light_store
+                        .insert(untrusted_block.clone(), Status::Unverified);
+                }
             }
-
-            // Don't storing since it's possible the light block is not valid
-            let untrusted_block = self.fetch_and_store_light_block(curr_end_block, false);
-
-            // Validate and verify the current block
-            let verdict = self.verifier.verify_update_header(
-                untrusted_block.as_untrusted_state(),
-                trusted_block.as_trusted_state(),
-                &self.options,
-                untrusted_block.time(),
-            );
-
-            // If valid light block, save the block to storage and iterate to the next height
-            if verdict == Verdict::Success {
-                self.state
-                    .light_store
-                    .insert(untrusted_block.clone(), Status::Verified);
-                return curr_end_block;
-            }
-
-            // If this block was not valid, this indicates that we cannot iterate to the next block
-            let mid_block = (curr_end_block + start_block) / 2;
-            curr_end_block = mid_block;
         }
+
+        return last_trusted;
     }
 
     /// Checking for a valid block that the light client can progress to within a given range of blocks.
