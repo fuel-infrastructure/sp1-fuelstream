2ff92299c9312dd28023fe48b6d5dc10b3d0c050
Dylan Frendo <dylanfrendo@gmail.com>
2024-12-23 18:32:40 +0900

    cometbft => Tendermint

--- script/src/light_client.rs
+++ script/src/light_client.rs
@@ -127,9 +127,9 @@ impl FuelStreamXLightClient {
         blocks
     }
 
-    /// Fetches a LightBlock from a CometBFT node. LightBlocks include validator sets.
+    /// Fetches a LightBlock from a Tendermint node. LightBlocks include validator sets.
     fn fetch_light_block(&mut self, block_height: u64) -> LightBlock {
-        debug!("fetching block {} from CometBFT", block_height);
+        debug!("fetching block {} from a Tendermint node", block_height);
         let error_msg = format!("could not request light block {}", block_height);
 
         self.io
@@ -177,7 +177,7 @@ mod tests {
         serde_json::from_str(&content).unwrap()
     }
 
-    // Helper function to set up FuelStreamXLightClient pointed to a mocked CometBFT server
+    // Helper function to set up FuelStreamXLightClient pointed to a mocked Tendermint node
     async fn setup_client_with_mocked_server(
         test_name: &'static str,
     ) -> (MockServer, FuelStreamXLightClient) {
