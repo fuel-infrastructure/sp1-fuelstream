a6499d27f2546531a001c2b526d5ab4dfd893bb1
Dylan Frendo <dylanfrendo@gmail.com>
2025-01-09 21:10:07 +0900

    testing autobuild

--- script/build.rs
+++ script/build.rs
@@ -4,8 +4,9 @@ use sp1_build::{build_program_with_args, BuildArgs};
 fn main() {
     let args = BuildArgs {
         docker: true,
+        tag: "v3.0.0".to_string(),
         elf_name: "fuelstreamx-elf".to_string(),
         ..Default::default()
     };
-    build_program_with_args("../program", &args);
+    build_program_with_args("../program", args);
 }
