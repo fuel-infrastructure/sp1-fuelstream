9a9e4387b1a59582a0bda6f5d842c33738896409
Dylan Frendo <dylanfrendo@gmail.com>
2025-01-17 21:15:46 +0900

    added tests

--- script/tests/operator_test.rs
+++ script/tests/operator_test.rs
@@ -11,6 +11,10 @@ mod tests {
     use crate::common::mock_tendermint_rpc_server::tests::spawn_tendermint_rpc_server;
     use crate::common::FULL_RUN;
 
+    use alloy::sol_types::SolType;
+
+    use primitives::types::ProofOutputs;
+
     use std::env;
 
     #[test]
@@ -48,7 +52,38 @@ mod tests {
             env::set_var("MINIMUM_BLOCK_RANGE", "15");
 
             let mut operator = FuelStreamXOperator::new().await;
-            operator.run().await;
+            let (proof_outputs, tx_hash) = operator.run().await.unwrap().unwrap();
+
+            assert_eq!(
+                "0xd8e75f208091e9bb60a26d8c51aa95cc2af007088cb00a54fba3abe5d6101a4d",
+                tx_hash.to_string()
+            );
+
+            // Abi-decode the public outputs
+            let (
+                trusted_height,
+                trusted_header_hash,
+                target_height,
+                target_header_hash,
+                bridge_commitment,
+            ) = ProofOutputs::abi_decode(&proof_outputs.public_values.to_vec(), true).unwrap();
+
+            // Check that the circuit public outputs are correct.
+            // The proof generated is assumed correct and handled by Succinct.
+            assert_eq!(177810, trusted_height);
+            assert_eq!(
+                "0x13416213335b27488b910bbfc1087740f0d3d9844af1b2ffd3eaf75433823ce1",
+                trusted_header_hash.to_string()
+            );
+            assert_eq!(177840, target_height);
+            assert_eq!(
+                "0x97b040e8c747f83e902ac6f046168c190db913a9eb813e987f1e748656239c3e",
+                target_header_hash.to_string()
+            );
+            assert_eq!(
+                "0xe3eeea6996acb801802718794b9611743671e7c33a7a83639e95382ed944f373",
+                bridge_commitment.to_string()
+            );
         });
     }
 }
