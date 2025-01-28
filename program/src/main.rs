92f66b4f5fb2f41ea4fe55317d2aa31a46177fde
Dylan Frendo <18212336+dferendo@users.noreply.github.com>
2025-01-28 23:06:05 +0900

    Update program/src/main.rs
    Co-authored-by: Miguel Dingli <migueldingli97@gmail.com>

--- program/src/main.rs
+++ program/src/main.rs
@@ -78,8 +78,8 @@ pub fn main() {
         ),
     }
 
-    // Compute the bridge commitment across the range. Target header is
-    // included here but excluded when computing the bridge commitment.
+    // Gather the headers for computing the bridge commitment across the block range.
+    // Target header included here but excluded when computing the bridge commitment.
     let mut all_headers = Vec::new();
     all_headers.push(trusted_light_block.signed_header.header.clone());
     all_headers.extend(headers);
